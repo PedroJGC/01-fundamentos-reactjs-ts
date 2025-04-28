ef18dc0b8e2d9bc9e47ec3a52dbc912e48d37735
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-04-28 18:08:05 -0300

    feat: - Add main application structure in App.jsx - Implement Header, Sidebar, Post, Comment, and Avatar components - Style components with respective CSS modules - Integrate date-fns for date formatting - Include global styles and variables - Add dependencies: date-fns and phosphor-react - Set up index.html and main.jsx for rendering

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import { Header } from './components/Header'
+import { Post } from './components/Post'
+import { Sidebar } from './components/Sidebar'
+
+import styles from './App.module.css'
+
+import './global.css'
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl:
+        'https://media.licdn.com/dms/image/v2/C4E03AQEgjZifrHUBfQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1617150701815?e=1750291200&v=beta&t=0r07TFh3RTLfrOR0IwMovsLBkCdWaVh-baLhOK7i_kk',
+      name: 'Pedro Carvalho',
+      role: 'Web developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      { type: 'link', content: '👉 jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2025-04-18 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO da @Rocketseat',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      { type: 'link', content: '👉 jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2025-03-28 20:00:00'),
+  },
+]
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Comment.jsx
@@ -0,0 +1,50 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import { useState } from 'react'
+import styles from './Comment.module.css'
+import { Avatar } from './Avatar'
+
+export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0)
+
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1
+    })
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src='https://github.com/diego3g.png' alt='' />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Pedro Carvalho</strong>
+              <time title='18 de Abril às 17:53' dateTime='2025-04-18 17:53:30'>
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button onClick={handleDeleteComment} title='Deletar comentário'>
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp size={20} />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
